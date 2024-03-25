73f9f4b2e6e1f2dc22f4be8d572fd4329abda874
Hippolyte GISSEROT-BOUKHLEF <hippolyte.gisserot-boukhlef@artefact.com>
2024-03-25 14:20:55 +0100

    save references as .txt

--- generate_sources.py
+++ generate_sources.py
@@ -13,6 +13,7 @@ def main():
     parser.add_argument('-s', '--split', type=str, default='train')
     parser.add_argument('-N', '--num-candidates', type=int, default=20)
     parser.add_argument('-sd', '--save-directory', type=str, default='data/train')
+    parser.add_argument('-gr', '--generate-refs', action='store_true')
     args = parser.parse_args()
 
     # Load data
@@ -22,34 +23,42 @@ def main():
     print('==========> Done.\n')
 
     # Generate source file
-    print('==========> Generating source file...')
-    
+    print('==========> Generating .txt files...')
+    sources = dataset['src']
+
+    try:
+        references = dataset['tgt']
+    except:
+        references = dataset['ref']
+
     if 'prompt' in dataset.features:
-        instructions = dataset['prompt']
-        instructions_rep = [prompt for prompt in instructions for _ in repeat(None, args.num_candidates)]
-        sources = dataset['src']
+        prompts = dataset['prompt']
+        prompts_rep = [prompt for prompt in prompts for _ in repeat(None, args.num_candidates)]
     else:    
-        sources = dataset['src']
         src_langs = dataset['src_lang']
         tgt_langs = dataset['tgt_lang']
-        instructions_rep = [] 
+        prompts_rep = [] 
         
         for src, src_lang, tgt_lang in tqdm(list(zip(sources, src_langs, tgt_langs))):
-            instructions_rep += [get_prompt(src, src_lang, tgt_lang).replace('\n', '\\n')] * args.num_candidates
+            prompts_rep += [get_prompt(src, src_lang, tgt_lang).replace('\n', '\\n')] * args.num_candidates
 
-    instructions_rep_txt = '\n'.join(instructions_rep)
+    prompts_rep_txt = '\n'.join(prompts_rep)
     sources_txt = '\n'.join(sources)
+    references_txt = '\n'.join(references)
     print('==========> Done.\n')
 
     # Save source files
-    print('==========> Saving source files...')
+    print('==========> Saving .txt files...')
     with open(f'{args.save_directory}/sources.txt', 'w') as f:
         f.write(sources_txt)     
     
     with open(
         f'{args.save_directory}/repeated_sources_N{args.num_candidates}.txt', 'w') as f:
-        f.write(instructions_rep_txt)
+        f.write(prompts_rep_txt)
     
+    with open(f'{args.save_directory}/references.txt', 'w') as f:
+        f.write(references_txt)     
+
     print('==========> Done.')
 
 
